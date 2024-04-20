7112bddf0d64df2eb4b60b917c8f599acb8360e9
Aviek Paul <aviek17@gmail.com>
2024-04-20 23:42:38 +0530

    SCA

--- SCA/src/components/Cart.jsx
+++ SCA/src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { CartContext } from "../store/shopping_cart_context";
 
-export default function Cart({ onUpdateItemQuantity }) {
+export default function Cart() {
 
   const cartCtx = useContext(CartContext);
 
@@ -26,11 +26,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => cartCtx.updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => cartCtx.updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
--- SCA/src/components/Product.jsx
+++ SCA/src/components/Product.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import  CartContext  from "../store/shopping_cart_context";
+import { CartContext } from "../store/shopping_cart_context";
 
 export default function Product({
   id,
